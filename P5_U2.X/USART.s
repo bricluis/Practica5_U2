767d86a8249fdc0e646ad3a67cba7297b813d595
Luis Briceno <luis.bric.lu@outlook.com>
2026-03-07 14:09:10 -0600

    Initial commit MPLAB

---
+++ P5_U2.X/USART.s
@@ -0,0 +1,89 @@
+; PIC16F877A Configuration Bit Settings
+  CONFIG  FOSC = XT
+  CONFIG  WDTE = OFF
+  CONFIG  PWRTE = OFF
+  CONFIG  BOREN = OFF
+  CONFIG  LVP = OFF
+  CONFIG  CPD = OFF
+  CONFIG  WRT = OFF
+  CONFIG  CP = OFF
+
+#include <xc.inc>
+ 
+W_TEMP      EQU 0x70
+STATUS_TEMP EQU 0x71
+REG1        EQU 0x72
+	
+PSECT   Code, delta=2
+        ORG     0x00
+        goto    INICIO
+
+        ORG     0x04
+	goto ISR
+    
+INICIO:
+    ; CONFIGURACION DE PUERTOS
+    bsf     STATUS, 5        ; cambiar al banco 1 (RP0 es el bit 5)
+    bcf     STATUS, 6        ; (RP1 es el bit 6)
+    clrf    TRISB            ; configurar puerto B como salida para los LEDs
+
+    ; CONFIGURACION DE PINES USART (RC6/TX entrada, RC7/RX entrada)
+    bsf     TRISC, 6
+    bsf     TRISC, 7
+
+    ; CONFIGURACION DE USART A 9600 BAUDIOS (ASUMIENDO CRISTAL DE 4MHz)
+    movlw   25            ; valor de SPBRG para 9600 bps con Fosc=4MHz y BRGH=1
+    movwf   SPBRG
+    bsf     TXSTA, 2         ; habilitar alta velocidad de baudios (BRGH es bit 2)
+    bcf     TXSTA, 4         ; configurar para modo asincrono (SYNC es bit 4)
+    bsf     TXSTA, 5         ; habilitar transmision (TXEN es bit 5)
+
+    ; HABILITAR INTERRUPCION DE RECEPCION
+    bsf     PIE1, 5          ; habilitar interrupcion por recepcion serial (RCIE es bit 5)
+
+    ; REGRESAR AL BANCO 0
+    bcf     STATUS, 5        ; (RP0 es el bit 5)
+
+    ; CONFIGURAR RECEPCION Y ENCENDER PUERTO SERIAL
+    bsf     RCSTA, 7         ; habilitar puerto serial (SPEN es bit 7)
+    bsf     RCSTA, 4         ; habilitar recepcion continua (CREN es bit 4)
+
+    ; LIMPIAR PORTB ANTES DE INICIAR
+    clrf    PORTB
+
+    ; CONFIGURACION DE INTERRUPCIONES GLOBALES
+    bsf     INTCON, 6        ; habilitar interrupciones de perifericos (PEIE es bit 6)
+    bsf     INTCON, 7        ; habilitar interrupciones globales (GIE es bit 7)
+
+LOOP:
+    ; BUCLE PRINCIPAL DE ESPERA
+    nop
+    goto    LOOP             ; dejar el USART escuchando y esperar interrupcion
+
+ISR:
+    ; GUARDAR CONTEXTO DE W Y STATUS
+    movwf   W_TEMP
+    swapf   STATUS, w
+    movwf   STATUS_TEMP
+
+    ; VERIFICAR SI LA INTERRUPCION FUE GENERADA POR RECEPCION
+    btfss   PIR1, 5          ; verificar la bandera de recepcion (RCIF es bit 5)
+    goto    SALIR_ISR
+    goto    DATORECIBIDO
+
+DATORECIBIDO:
+    ; LEER, MOSTRAR EN LEDS Y TRANSMITIR COMO ECO
+    movf    RCREG, w         ; leer dato recibido (esto limpia bandera RCIF)
+    movwf   REG1             ; guardar dato en variable temporal
+    movwf   PORTB            ; mostrar el dato en forma binaria en el puerto B
+    movwf   TXREG            ; retransmitir el mismo dato a la PC como ECO
+
+SALIR_ISR:
+    ; RESTAURAR CONTEXTO DE STATUS Y W Y SALIR
+    swapf   STATUS_TEMP, w
+    movwf   STATUS
+    swapf   W_TEMP, f
+    swapf   W_TEMP, w
+    retfie                   ; retornar de la interrupcion y volver al bucle
+
+    END
---
+++ P5_U2.X/nbproject/Makefile-default.mk
@@ -0,0 +1,210 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=USART.s main.s SPI.s ADC.s PWM.s
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/USART.o ${OBJECTDIR}/main.o ${OBJECTDIR}/SPI.o ${OBJECTDIR}/ADC.o ${OBJECTDIR}/PWM.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/USART.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/SPI.o.d ${OBJECTDIR}/ADC.o.d ${OBJECTDIR}/PWM.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/USART.o ${OBJECTDIR}/main.o ${OBJECTDIR}/SPI.o ${OBJECTDIR}/ADC.o ${OBJECTDIR}/PWM.o
+
+# Source Files
+SOURCEFILES=USART.s main.s SPI.s ADC.s PWM.s
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=PIC16F877A
+FINAL_IMAGE_NAME_MINUS_EXTENSION=${DISTDIR}/P5_U2.X.${IMAGE_TYPE}
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: pic-as-assembler
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/USART.o: USART.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/USART.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/USART.o \
+	USART.s \
+	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/main.o: main.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/main.o \
+	main.s \
+	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/SPI.o: SPI.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/SPI.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/SPI.o \
+	SPI.s \
+	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/ADC.o: ADC.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ADC.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/ADC.o \
+	ADC.s \
+	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/PWM.o: PWM.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PWM.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/PWM.o \
+	PWM.s \
+	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+else
+${OBJECTDIR}/USART.o: USART.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/USART.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/USART.o \
+	USART.s \
+	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/main.o: main.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/main.o \
+	main.s \
+	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/SPI.o: SPI.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/SPI.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/SPI.o \
+	SPI.s \
+	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/ADC.o: ADC.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ADC.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/ADC.o \
+	ADC.s \
+	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+${OBJECTDIR}/PWM.o: PWM.s  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/PWM.o 
+	${MP_AS} -mcpu=PIC16F877A -c \
+	-o ${OBJECTDIR}/PWM.o \
+	PWM.s \
+	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: pic-as-linker
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} ${DISTDIR} 
+	${MP_LD} -mcpu=PIC16F877A ${OBJECTFILES_QUOTED_IF_SPACED} \
+	-o ${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} \
+	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -mcallgraph=std -Wl,-Map=${FINAL_IMAGE_NAME_MINUS_EXTENSION}.map -mno-download-hex
+else
+${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} ${DISTDIR} 
+	${MP_LD} -mcpu=PIC16F877A ${OBJECTFILES_QUOTED_IF_SPACED} \
+	-o ${DISTDIR}/P5_U2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} \
+	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -mcallgraph=std -Wl,-Map=${FINAL_IMAGE_NAME_MINUS_EXTENSION}.map -mno-download-hex
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(wildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
